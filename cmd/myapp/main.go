ea1bda4aaf1348bba4ae2fdb3eef3cfd808371fb
Andy C <andy.cookson@nexusrental.co.uk>
2024-11-12 14:35:49 +0000

    main script

--- cmd/myapp/main.go
+++ cmd/myapp/main.go
@@ -4,6 +4,36 @@ import (
     "fmt"
 )
 
+type Animal interface {
+    Speak() string
+}
+
+type Dog struct {}
+type Cat struct {}
+
+func(d Dog) Speak() string { return "Woof!" }
+func(c Cat) Speak() string { return "Meow!" }
+
+type Zoo struct {
+    animal Animal
+}
+
+func(z Zoo) MakeAnimalSpeak(){
+    fmt.Println(z.animal.Speak())
+}
+
 func main() {
-    fmt.Println("interfaces and polymorphism")
+    fmt.Println("interfaces and polymorphism - begin")
+
+    zooWithDog := Zoo{animal: Dog{}}
+    zooWithDog.MakeAnimalSpeak()
+
+    zooWithCat := Zoo{animal: Cat{}}
+    zooWithCat.MakeAnimalSpeak()
+
+
+
+
+
+
 }
