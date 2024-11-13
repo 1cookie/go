e8b78feedd8cb6b013e0e44775ed07e373eb25a4
Andy C <andy.cookson@nexusrental.co.uk>
2024-11-13 15:52:25 +0000

    updated

--- cmd/myapp/main.go
+++ cmd/myapp/main.go
@@ -3,18 +3,26 @@ package main
 import (
     "fmt"
     "os"
+    "net/http"
+    "io/ioutil"
 )
 
 func main() {
 
     file, err := os.Create("example.txt")
+    resp, err := http.Get("https://httpbin.org/get")
+
     if err != nil {
         fmt.Println("Error;", err)
         return
     }
     defer file.Close()
+    defer resp.Body.Close()
 
     file.WriteString("Hello, Go!")
     data, _ := os.ReadFile("example.txt")
     fmt.Println("File Content:", string(data))
+
+    body, _ := ioutil.ReadAll(resp.Body)
+    fmt.Println("Response:", string(body))
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/1cookie/go
+
+go 1.21.1
